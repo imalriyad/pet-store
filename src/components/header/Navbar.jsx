0282d1dcc2917774f3fe74ad183d6e91495e2da7
AL Riyad <imalriyad@gmail.com>
2024-04-06 00:58:22 +0600

    navbar update

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,16 +1,10 @@
 "use client";
-import { Navbar, Input } from "keep-react";
-import {
-  CaretDown,
-  FacebookLogo,
-  InstagramLogo,
-  MagnifyingGlass,
-  TwitterLogo,
-} from "phosphor-react";
+import { Navbar } from "keep-react";
+import { CaretDown } from "phosphor-react";
 
 export const NavbarComponent = () => {
   return (
-    <Navbar fluid={true} className="border px-0" >
+    <Navbar fluid={true} className="border px-0">
       <Navbar.Container className="flex items-center justify-between">
         <Navbar.Brand>
           <img src="/src/assets/logo.png" alt="logo" className="w-[60%]" />
@@ -21,33 +15,53 @@ export const NavbarComponent = () => {
             tag="ul"
             className="lg:flex hidden items-center justify-between gap-4"
           >
-            <Input
-              id="#id-10"
-              placeholder="Search anything"
-              color="gray"
-              sizing="sm"
-              type="text"
-              addon={<MagnifyingGlass size={20} color="#5E718D" />}
-              addonPosition="left"
-            />
-            <Navbar.Link
-              icon={<FacebookLogo size={20} color="#444" />}
-              iconAnimation={false}
-            />
-            <Navbar.Link
-              icon={<InstagramLogo size={20} color="#444" />}
-              iconAnimation={false}
-            />
-            <Navbar.Link
-              icon={<TwitterLogo size={20} color="#444" />}
-              iconAnimation={false}
-            />
+            {/* Input feild */}
+            <div className="lg:w-[400px] xl:w-[600px] mx-auto">
+              <div>
+                <div className="group relative">
+                  <input
+                    type="text"
+                    id="example9"
+                    className="block w-full rounded-sm border focus:outline-none border-gray-300 px-4 pr-8 py-2 "
+                    placeholder="Search for more than 10,000 products"
+                  />
+
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2.5 ">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="h-5 w-5"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
           </Navbar.Container>
+
           <Navbar.Container className="flex gap-1">
             <Navbar.Toggle className="block" />
-           
           </Navbar.Container>
         </Navbar.Container>
+
+        {/* Nav Info */}
+        <Navbar.Container className="lg:flex hidden list-none justify-between gap-6 items-center">
+          <div className="py-6">
+            <p className="text-[#908F8D] text-sm text-right">Phone</p>
+            <p className="text-[#41403E] text-sm">+980-34984089</p>
+          </div>
+          <div className="py-6">
+            <p className="text-[#908F8D] text-sm text-right">Email</p>
+            <p className="text-[#41403E] text-sm">waggy@gmail.com</p>
+          </div>
+        </Navbar.Container>
+
         <Navbar.Collapse
           collapseType="sidebar"
           className="fixed right-0 top-0 bg-white p-10 lg:!w-2/6 xl:!w-1/6 md:!w-2/6 w-1/2"
